ff86d026366335dc9c5b96d4e2b9531f53784470
Jorge Pereira <jpereiran@gmail.com>
2014-11-04 13:56:43 -0200

    Added wrapper for convert array to json

--- class.DhcpdLeases.php
+++ class.DhcpdLeases.php
@@ -187,63 +187,52 @@ class DhcpdLeases {
      */
     function GetResultJson()
     { 
-        if (function_exists('json_encode'))
+        if (function_exists("json_encode"))
             return json_encode($this->row_array);
+        else
+            return $this->wrapper_array2json($this->row_array);
+    }
 
+    /**
+     * wrapper used to convert array to json
+     */
+    private function wrapper_array2json($arr)
+    { 
         $parts = array(); 
         $is_list = false; 
-
-        $keys = array_keys($this->row_array); 
-        $max_length = count($this->row_array)-1; 
-        if (($keys[0] == 0) and ($keys[$max_length] == $max_length))
-        {
+        //Find out if the given array is a numerical array 
+        $keys = array_keys($arr); 
+        $max_length = count($arr)-1; 
+        if(($keys[0] == 0) and ($keys[$max_length] == $max_length)) {//See if the first key is 0 and last key is length - 1
             $is_list = true; 
-            for ($i=0; $i < count($keys); $i++)
-            {
-                if ($i != $keys[$i])
-                {
-                    $is_list = false;
+            for($i=0; $i<count($keys); $i++) { //See if each key correspondes to its position 
+                if($i != $keys[$i]) { //A key fails at position check. 
+                    $is_list = false; //It is an associative array. 
                     break; 
                 } 
             } 
         } 
-
-        foreach ($this->row_array as $key=>$value)
-        { 
-            if (is_array($value))
-            {
-                if ($is_list)
-                    $parts[] = array2json($value); /* :RECURSION: */ 
-                else
-                    $parts[] = '"' . $key . '":' . array2json($value); /* :RECURSION: */ 
-            }
-            else
-            { 
+        foreach($arr as $key=>$value) { 
+            if(is_array($value)) { //Custom handling for arrays 
+                if($is_list) $parts[] = $this->wrapper_array2json($value); /* :RECURSION: */ 
+                else $parts[] = '"' . $key . '":' . $this->wrapper_array2json($value); /* :RECURSION: */ 
+            } else { 
                 $str = ''; 
-                if(!$is_list)
-                    $str = '"' . $key . '":'; 
-
-                // Custom handling for multiple data types 
-                if(is_numeric($value))
-                    $str .= $value;   // Numbers 
-                elseif($value === false)
-                    $str .= 'false'; // The booleans 
-                elseif($value === true)
-                    $str .= 'true'; 
+                if(!$is_list) $str = '"' . $key . '":'; 
+                //Custom handling for multiple data types 
+                if(is_numeric($value)) $str .= $value; //Numbers 
+                elseif($value === false) $str .= 'false'; //The booleans 
+                elseif($value === true) $str .= 'true'; 
                 else $str .= '"' . addslashes($value) . '"'; //All other things 
                 // :TODO: Is there any more datatype we should be in the lookout for? (Object?) 
-
                 $parts[] = $str; 
             } 
         } 
         $json = implode(',',$parts); 
          
         if ($is_list)
-            return '[' . $json . ']'; // Return numerical JSON 
-
-        return '{' . $json . '}';     // Return associative JSON 
-    } 
-}
-
+            return '[' . $json . ']';//Return numerical JSON 
+        return '{' . $json . '}';//Return associative JSON 
+    }
+} /* class DhcpdLeases */
 ?>
-
