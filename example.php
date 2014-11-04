7c046d3a8a4a579be0962fec6f455c46ec0d21c4
Jorge Pereira <jpereiran@gmail.com>
2014-11-03 22:47:46 -0200

    better example using json() result

--- example.php
+++ example.php
@@ -1,21 +1,85 @@
 <?php
-/* sample using class.DhcpLeases.php */
-
+/**
+ *  This file is part of class.DhcpLeases.php
+ *
+ *    class.DhcpLeases.php is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU Lesse General Public License as published by the Free Software Foundation, either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  class.DhcpLeases.php is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesse General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesse General Public License
+ *  along with class.DhcpLeases.php.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (C) 2014, Jorge Pereira <jpereiran@gmail.com>
+ */
 require_once("class.DhcpdLeases.php");
 
-$dl = new DhcpdLeases("dhcpd.leases");
+function array2json($arr)
+{ 
+    if(function_exists('json_encode'))
+        return json_encode($arr); //Lastest versions of PHP already has this functionality.
 
-$dl->setFilter("hw", "9c:65:b0:c4:d7:aa");
+    $parts = array(); 
+    $is_list = false; 
 
-if (!$dl->process())
+    //Find out if the given array is a numerical array 
+    $keys = array_keys($arr); 
+    $max_length = count($arr)-1; 
+    if(($keys[0] == 0) and ($keys[$max_length] == $max_length)) {//See if the first key is 0 and last key is length - 1
+        $is_list = true; 
+        for($i=0; $i<count($keys); $i++) { //See if each key correspondes to its position 
+            if($i != $keys[$i]) { //A key fails at position check. 
+                $is_list = false; //It is an associative array. 
+                break; 
+            } 
+        } 
+    } 
+
+    foreach($arr as $key=>$value) { 
+        if(is_array($value)) { //Custom handling for arrays 
+            if($is_list) $parts[] = array2json($value); /* :RECURSION: */ 
+            else $parts[] = '"' . $key . '":' . array2json($value); /* :RECURSION: */ 
+        } else { 
+            $str = ''; 
+            if(!$is_list) $str = '"' . $key . '":'; 
+
+            //Custom handling for multiple data types 
+            if(is_numeric($value)) $str .= $value; //Numbers 
+            elseif($value === false) $str .= 'false'; //The booleans 
+            elseif($value === true) $str .= 'true'; 
+            else $str .= '"' . addslashes($value) . '"'; //All other things 
+            // :TODO: Is there any more datatype we should be in the lookout for? (Object?) 
+
+            $parts[] = $str; 
+        } 
+    } 
+    $json = implode(',',$parts); 
+     
+    if ($is_list)
+        return '[' . $json . ']';//Return numerical JSON 
+
+    return '{' . $json . '}';//Return associative JSON 
+} 
+
+// main()
+$dl = new DhcpdLeases();
+
+if (!@$_GET['hw'])
+    die("need to set \$_GET['hw']");
+
+$dl->setFilter("hw", strtolower($_GET['hw']));
+
+if ($dl->process() < 1)
 {
     echo "Not Found!";
 }
 else
 {
     $arr = $dl->GetResult();
-    echo "<pre>";
-    print_r($arr);
-    echo "</pre>";
+    echo array2json($arr);
 }
 ?>
